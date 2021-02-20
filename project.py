b2e8ee1af6af62d072686982e5fc86127f55159b
jonathanchen985 <jonathanchen1999@gmail.com>
2021-02-20 15:53:57 -0500

    Update

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 #Project file
 import pandas as pd 
+import re
 
 df = pd.read_csv ("Data_Lv2_USDA_PackagedMeals.csv")
 
@@ -9,7 +10,7 @@ print(ingredients)
 for item in df["ingredients"]:
     item= str(item)
     item=item.split(",")
-    print(item)
+    
    
 
 
