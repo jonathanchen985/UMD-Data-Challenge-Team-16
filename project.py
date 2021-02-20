4791ab76f17b9e25795a245379e6b0de1aa6f2cd
Jonathan Chen <jonathanchen0510@gmail.com>
2021-02-20 15:07:17 -0500

    t

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
-#Project file
-import pandas as pd 
 
-df=pd.read_excel (r"Data_Lv2_USDA_PackagedMeals-xlsx.xls")
-print (df)
+import pandas as pd 
+import numpy as np
+
+#import data
+df = pd.read_csv("Data_Lv2_USDA_PackagedMeals.csv") 
+
+
 
