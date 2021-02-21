5337c7b2761d8542482dd45c3cbe0d2d867c6476
Jonathan Chen <jonathanchen0510@gmail.com>
2021-02-20 23:04:37 -0500

    finished getting top ingredients

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 #Import Packages
 import pandas as pd 
 import re
+from collections import Counter 
 # Read in cleaned dataset
 df = pd.read_csv ("Data_Lv2_USDA_PackagedMeals.csv")
 
@@ -12,6 +13,7 @@ myList=[]
 for item in ingredients:
     item= str(item)
     myList.append(item)
+#cast to str so can use in function
 myListStr=str(myList)    
 
 #Function: Remove items within parentheses, brackects and curly brackets
@@ -32,9 +34,19 @@ def remove_char(test_str):
             ret += i
     return ret
 
-#Remove '*' in myList 
-ingredient_list = remove_char(myListStr)
-print(ingredient_list.replace("*",""))
+#Remove () and []
+ingredient_listStr = remove_char(myListStr)
+#Remove *
+ingredient_listStr_final=ingredient_listStr.replace("*","")
+
+# split() returns list of all the words in the string
+split_it = ingredient_listStr_final.split(",")
+# Pass the split_it list to instance of Counter class.
+Counter = Counter(split_it)
+# input values and their respective counts.
+most_occur = Counter.most_common(5)
+print(f"Most common ingredients and how many times occur: {most_occur}")
+
 
 
    
