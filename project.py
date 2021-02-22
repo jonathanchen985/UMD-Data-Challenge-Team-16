b559f624c1da08771b7565d9dd1300ced235754e
Jonathan Chen <jonathanchen0510@gmail.com>
2021-02-22 01:16:31 -0500

    fixed counter

--- project.py
+++ project.py
@@ -40,13 +40,11 @@ ingredient_listStr = remove_char(myListStr)
 ingredient_listStr_final=ingredient_listStr.replace("*","")
 
 # split() returns list of all the words in the string
-split_it = ingredient_listStr_final.split()
-# Pass the split_it list to instance of Counter class.
-Counter = Counter(split_it)
-# input values and their respective counts.
-most_occur = Counter.most_common(5)
-print(f"Most common ingredients and how many times occur: {most_occur}")
-
+split_it = ingredient_listStr_final.split(",")
+print(split_it)
+words_to_count = (word for word in split_it)
+c = Counter(words_to_count)
+print (f"Most common ingredients and how many times occur: {c.most_common(10)}")
 
 
 
