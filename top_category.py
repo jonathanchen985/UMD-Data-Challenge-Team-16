d2fda6e46eb6f05d0e3101d30cfffb509ae0fe02
Jonathan Chen <jonathanchen0510@gmail.com>
2021-02-24 22:57:24 -0500

    made new file for category

---
+++ top_category.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+
+#remove () and [] from string
+def remove_char(test_str):
+    test_str = str(test_str)
+    ret = ''
+    skip1c = 0
+    skip2c = 0
+    for i in test_str:
+        if i == '[':
+            skip1c += 1
+        elif i == '(':
+            skip2c += 1
+        elif i == ']' and skip1c > 0:
+            skip1c -= 1
+        elif i == ')'and skip2c > 0:
+            skip2c -= 1
+        elif skip1c == 0 and skip2c == 0:
+            ret += i
+    return ret
+
+#select category for analyzation
+def select_data_category(data, category):
+    cat = data[category].unique()
+    print('Select from the following categories')
+    for idx,value in enumerate(cat):
+        print(f'Enter {idx} to select {value}')
+    selection = int(input('Selection: '))
+    return data[data[category]==cat[selection]]
+
+#read in csv
+df = pd.read_csv ("Data_Lv2_USDA_PackagedMeals.csv")
+#run line to analyze a single food category
+df2 = select_data_category(df,'branded_food_category')
+#apply remove_char function
+df2['ingredients'] = df2['ingredients'].apply(remove_char)
+
+#create list for all ingredients
+all_ingredients = [] 
+for row in df2['ingredients']:
+    #split each ingredient at comma
+    temp = row.split(',')
+    #remove leading and trailing whitespace
+    temp = list(map(str.strip,temp))
+    #remove * and .
+    all_ingredients += list(map(lambda _: _.strip('*.'),temp))
+#make series    
+all_ingredients = pd.Series(map(str.strip,all_ingredients))
+#get series of ingredients and their counts
+count = all_ingredients.value_counts()
+#print top ingredients and their counts
+print(f"CATEGORY TOP INGREDIENTS \n{count[:10]}")
+#plot top ingredients (MAKE SURE TO DOWNLOAD PYTHON EXTENSION PACK IF IN VSC)
+count[:10].plot(kind='bar')
+plt.show()
+
