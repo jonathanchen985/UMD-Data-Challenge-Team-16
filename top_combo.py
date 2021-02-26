39f3313d338aae6c4e227705c5b14df5ebc7819d
jonathanchen985 <jonathanchen1999@gmail.com>
2021-02-26 12:02:26 -0500

    Updated top_combo.py

--- top_combo.py
+++ top_combo.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import itertools
-import random
-
+from itertools import combinations
+import random 
+from collections import Counter
 #remove () and [] from string
 def remove_char(test_str):
     test_str = str(test_str)
@@ -23,20 +23,36 @@ def remove_char(test_str):
 
 #read in csv
 df = pd.read_csv ("Data_Lv2_USDA_PackagedMeals.csv")
-#generate random numbers for use in subset
-num1=random.randint(0,4439)
-num2=num1-100
-if(num2<0):
-    num2=num1+100
-#apply remove_char function to random 100 subset of data
-df2 = df['ingredients'].apply(remove_char)[num2:num1]
 
-#get combinations
-combinations_list = []
-for row in df2:
-    combinations = list(itertools.combinations(df2, 2))
-    combinations_list.append(combinations)
+#Create another dataframe
+df2 = df['ingredients'].apply(remove_char)
 
-combination_counts = pd.Series(combinations_list).explode().reset_index(drop=True)
-#print top combinations
-print(f"TOP COMBINATIONS \n{combination_counts.value_counts()[:3]}")
+#create list for all ingredients
+lst2=[]
+
+lst = df2.to_list()
+# Clean up data and put into another list
+for i in range(len(lst)):
+    #split each ingredient at comma
+    temp = lst[i].split(',')
+    #remove leading and trailing whitespace
+    temp = list(map(str.strip,temp))
+    #remove * and .
+    temp = list(map(lambda _: _.strip('*.'),temp))
+    lst2.append(temp)
+    lst[i] = ",".join(temp)
+
+#Combination Function
+def collect_pairs(lines, token_count):
+    pair_counter = Counter()
+    for line in lines:
+        unique_tokens = sorted(set(line))  # exclude duplicates in same line and sort to ensure one word is always before other
+        combos = combinations(unique_tokens, token_count)
+        pair_counter += Counter(combos)
+    return pair_counter
+
+#Get the combinations of ingredients     
+pairs1 = collect_pairs(lst2, 3)
+print(pairs1.most_common(10))
+
+#print(f"TOP COMBINATIONS \n{combination_counts.value_counts()[:3]}")
